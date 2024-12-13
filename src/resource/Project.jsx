da19bbb5f6a2178866f8bf627e96296604616bcf
rozaANDSO <belayroza408@gmail.com>
2024-12-13 02:59:18 -0800

    Update Project.jsx

--- src/resource/Project.jsx
+++ src/resource/Project.jsx
@@ -1,18 +1,18 @@
 export const project=[
     {
         title:"E-commerce Website",
-        link:"https://eccomerce-website-o2rt-b2x6oi6l1-rozas-projects-2ede3e84.vercel.app/",
+        link:"https://eccomerce-website.vercel.app/",
         description:"An e-commerce website built using React, Redux, and Tailwind CSS. Users can browse products, add items to the cart, and checkout.",
     
     },
     {
         title:"Book Store",
-        link:"https://book-store-zc6n-9tnqdgy6n-rozas-projects-2ede3e84.vercel.app/",
+        link:"https://book-store-.vercel.app/",
         description:"A book store website built using React, Redux, and Next.js. Users can browse products, add items to the cart, and checkout.",
     
     },
     {
         title:"ludo game",
-        link:""
+        link:"https://ludo-flame.vercel.app/"
     }
 ]
