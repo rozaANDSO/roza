0452508aa93a9c45d48a4a3f8c51cbcfa8d16702
rozaANDSO <belayroza408@gmail.com>
2024-12-19 12:34:06 -0800

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <div className='p-5  flex justify-between'>
+    <div className='p-5 flex justify-between'>
         <h1 className=' text-red-400 text-4xl font-semibold'>R</h1>
         <h1 className=' text-black text-4xl font-semibold'>B</h1>
         <h1 className=' text-green-400 text-4xl font-semibold'>T</h1>
