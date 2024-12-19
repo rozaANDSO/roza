9b0bd27f04273712ec68fee9eebcc78f708e39c5
rozaANDSO <belayroza408@gmail.com>
2024-12-19 12:37:29 -0800

    Update SectionTitle.jsx

--- src/component/SectionTitle.jsx
+++ src/component/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({title}) => {
   return (
     <div className='flex gap-10 items-center'>
         <h1 className='text-2xl text-tertiary font-semibold'>{title}</h1>
-        <div className="w-40 h-1 bg-black"></div>
+        <div className="w-40 h-1 "></div>
       </div>
 
   )
