92a867f1113ae16699a9ca0fba56440bd82af29f
rozaANDSO <belayroza408@gmail.com>
2024-12-19 12:31:49 -0800

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
     <div className='p-5  flex justify-between'>
         <h1 className=' text-red-400 text-4xl font-semibold'>R</h1>
-        <h1 className=' text-white text-4xl font-semibold'>B</h1>
+        <h1 className=' text-black text-4xl font-semibold'>B</h1>
         <h1 className=' text-green-400 text-4xl font-semibold'>T</h1>
 
 
