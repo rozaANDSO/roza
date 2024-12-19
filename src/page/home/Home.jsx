cadd0c82ec195459bea486557084c585cc104649
rozaANDSO <belayroza408@gmail.com>
2024-12-19 12:27:43 -0800

    Update Home.jsx

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     <>
       <div>
         <Header />
-        <div className="px-40 w-full">
+        <div className="px-40 ">
           <Intro />
           <About />
           <Expricence />
